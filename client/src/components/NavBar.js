5ae8f5dc5010c69e76039abaee205a143de84ba4
Treven Trujillo <treventrujillo@gmail.com>
2017-11-01 12:04:36 -0600

    updated navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -10,11 +10,27 @@ import { connect } from 'react-redux';
 import { handleLogout } from '../actions/auth';
 
 const NavBar = () => (
-  <Menu>
+  //TODO: Make links active 
+  <Menu style={styles.bar} borderless>
     <Menu.Item position='right'>
-      <Input className='icon' icon='search' placeholder='Search...' />
+      <Input className='icon' icon='search' placeholder='Search...' size='huge' />
+    </Menu.Item>
+    <Menu.Item>
+      <Icon className='alarm' size='big' />
+    </Menu.Item>
+    <Menu.Item>
+      <Icon className='users' size='big' />
+    </Menu.Item>
+    <Menu.Item>
+      <Icon className='user circle' size='big' />
     </Menu.Item>
   </Menu>
 )
+
+const styles = {
+  bar : {
+    'height': '15vh',
+  },
+}
 
 export default NavBar;
