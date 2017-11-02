4d7fe869012a7746f6194f80d8e126b99e717f12
Treven Trujillo <treventrujillo@gmail.com>
2017-11-01 18:01:36 -0600

    updated to latest ruby version, started creating mailers, set root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
+
+  root to: 'static#index'
+  
   namespace :api do
-    #API ROUTES SHOULD GO HERE
+  
   end
 
   #Do not place any routes below this one
