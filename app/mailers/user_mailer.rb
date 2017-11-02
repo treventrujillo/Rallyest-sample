911c3c9b76161902df39701a5f30b6e5ba3cda1e
Treven Trujillo <treventrujillo@gmail.com>
2017-11-01 18:31:59 -0600

    rough drafted mailers, still not functioning

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
-class UserMailer < ApplicationMailer  
+class UserMailer < ApplicationMailer
+  default from: 'verification@rallyest.com'  
+  
+  def verification_email(user) 
+    @user = user
+    @url = 'http://localhost:3000/login'
+    mail(to: @user.email, subject: 'Confirm your account')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
-
   root to: 'static#index'
   
   namespace :api do
