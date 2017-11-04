1f8b5c0a2977de82f933bd7d6ae6a101d3c5f42b
Erik McComb <mccomb@Eriks-MacBook-Pro.local>
2017-11-04 15:47:12 -0600

    Working left nav - links working

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -12,7 +12,17 @@ import LeftMenu from './LeftMenu';
 // Pages in Switch
 import NoMatch from './NoMatch';
 import Home from './Home';
+import Files from './Files';
 import Login from './Login';
+import Photos from './Photos';
+import Letters from './Letters';
+import Goals from './Goals';
+import Courses from './Courses';
+import Announcements from './Announcements';
+import Updates from './Updates';
+import Assignments from './Assignments';
+import Community from './Community';
+import Settings from './Settings';
 import Register from './Register';
 
 class App extends Component {
@@ -28,6 +38,16 @@ class App extends Component {
           <FetchUser>
             <Switch>
               <Route exact path='/' component={Home} />
+              <Route path='/Files' component={Files} />
+              <Route path='/Photos' component={Photos} />
+              <Route path='/Letters' component={Letters} />
+              <Route path='/Goals' component={Goals} />
+              <Route path='/Courses' component={Courses} />
+              <Route path='/Announcements' component={Announcements} />
+              <Route path='/Updates' component={Updates} />
+              <Route path='/Assignments' component={Assignments} />
+              <Route path='/Community' component={Community} />
+              <Route path='/Settings' component={Settings} />
               <AuthRoute exact path='/login' component={Login} />
               <AuthRoute exact path='/register' component={Register} />
               <Route component={NoMatch} />
--- client/src/components/LeftMenu.js
+++ client/src/components/LeftMenu.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Menu } from 'semantic-ui-react'
+import {Link} from 'react-router-dom'
 
 class LeftMenu extends Component {
   state = { activeItem: 'Rallyest' }
@@ -12,49 +13,49 @@ class LeftMenu extends Component {
     return (
       <div >
         <div style={styles.menu}>
-          <Menu pointing secondary vertical >           
-            <Menu.Item active={activeItem === 'Rallyest'} onClick={this.handleItemClick}>
-              RALLYEST
+          <Menu pointing secondary vertical>           
+            <Menu.Item name='Rallyest' active={activeItem === 'Rallyest'} onClick={this.handleItemClick}>
+              <Link to={'/'} style={{color: '#e1e6e7'}}>RALLYEST</Link>
             </Menu.Item>
 
-            <Menu.Item name='Messages' active={activeItem === 'Messages'} onClick={this.handleItemClick}>
-
+            <Menu.Item name='Files' active={activeItem === 'Files'} onClick={this.handleItemClick}>
+              <Link to={'/Files'} style={{color: '#e1e6e7'}}>Files</Link>
             </Menu.Item>
 
             <Menu.Item name='Photos' active={activeItem === 'Photos'} onClick={this.handleItemClick}>
-
+              <Link to={'/Photos'} style={{color: '#e1e6e7'}}>Photos</Link>
             </Menu.Item>
 
             <Menu.Item name='Letters' active={activeItem === 'Letters'} onClick={this.handleItemClick}>
-
+              <Link to={'/Letters'} style={{color: '#e1e6e7'}}>Letters</Link>
             </Menu.Item>
 
             <Menu.Item name='Goals' active={activeItem === 'Goals'} onClick={this.handleItemClick}>
-
+              <Link to={'/Goals'} style={{color: '#e1e6e7'}}>Goals</Link>
             </Menu.Item>
            
             <Menu.Item name='Courses' active={activeItem === 'Courses'} onClick={this.handleItemClick}>
-
+              <Link to={'/Courses'} style={{color: '#e1e6e7'}}>Courses</Link>
             </Menu.Item>
             
             <Menu.Item name='Announcements' active={activeItem === 'Announcements'} onClick={this.handleItemClick}>
-
+              <Link to={'/Announcements'} style={{color: '#e1e6e7'}}>Announcements</Link>
             </Menu.Item>
            
             <Menu.Item name='Updates' active={activeItem === 'Updates'} onClick={this.handleItemClick}>
-
+              <Link to={'/Updates'} style={{color: '#e1e6e7'}}>Updates</Link>
             </Menu.Item>
             
             <Menu.Item name='Assignments' active={activeItem === 'Assignments'} onClick={this.handleItemClick}>
-
+              <Link to={'/Assignments'} style={{color: '#e1e6e7'}}>Assignements</Link>
             </Menu.Item>
            
             <Menu.Item name='Community' active={activeItem === 'Community'} onClick={this.handleItemClick}>
-
+              <Link to={'/Community'} style={{color: '#e1e6e7'}}>Community</Link>
             </Menu.Item>
            
             <Menu.Item name='Settings' active={activeItem === 'Settings'} onClick={this.handleItemClick}>
-
+              <Link to={'/Settings'} style={{color: '#e1e6e7'}}>Settings</Link>
             </Menu.Item>
           </Menu>
         </div>
