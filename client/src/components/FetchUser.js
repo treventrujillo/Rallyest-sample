5f65a2aa0f76e25f0c4868a7a9ecaf8898d27b5d
Treven Trujillo <treventrujillo@gmail.com>
2017-11-05 15:13:39 -0700

    commnented out fetchuser component

--- client/src/components/FetchUser.js
+++ client/src/components/FetchUser.js
@@ -1,31 +1,31 @@
-import { Component } from 'react';
-import { connect } from 'react-redux';
-import { validateToken } from '../actions/auth';
+// import { Component } from 'react';
+// import { connect } from 'react-redux';
+// import { validateToken } from '../actions/auth';
 
-class FetchUser extends Component {
-  state = { loaded: false };
+// class FetchUser extends Component {
+//   state = { loaded: false };
 
-  componentDidMount() {
-    const { isAuthenticated, dispatch } = this.props;
-    if (isAuthenticated) this.loaded();
-    else dispatch(validateToken(this.loaded));
-  }
+//   componentDidMount() {
+//     const { isAuthenticated, dispatch } = this.props;
+//     if (isAuthenticated) this.loaded();
+//     else dispatch(validateToken(this.loaded));
+//   }
 
-  componentWillReceiveProps() {
-    if (!this.state.loaded) this.loaded();
-  }
+//   componentWillReceiveProps() {
+//     if (!this.state.loaded) this.loaded();
+//   }
 
-  loaded = () => {
-    this.setState({ loaded: true });
-  }
+//   loaded = () => {
+//     this.setState({ loaded: true });
+//   }
 
-  render() {
-    return this.state.loaded ? this.props.children : null;
-  }
-}
+//   render() {
+//     return this.state.loaded ? this.props.children : null;
+//   }
+// }
 
-const mapStateToProps = state => {
-  return { isAuthenticated: state.user.id };
-};
+// const mapStateToProps = state => {
+//   return { isAuthenticated: state.user.id };
+// };
 
-export default connect(mapStateToProps)(FetchUser);
+// export default connect(mapStateToProps)(FetchUser);
