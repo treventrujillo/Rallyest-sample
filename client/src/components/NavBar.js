b9ae8c737192754b1224a2650e06c66b1c03b260
Patrick Seiuli <patrickseiuli@Patricks-MacBook-Air.local>
2017-11-07 13:29:54 -0700

    navbar image added

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -11,29 +11,33 @@ import { connect } from 'react-redux';
 import { handleLogout } from '../actions/auth';
 import { NavWrap } from './styles/styles';
 
-const options = [
-  { value: '0', text: <span><Icon name='protect' /> Team 1</span> },
-  { value: '1', text: <span><Icon name='protect'/> Team 2</span> },
-  { value: '2', text: <span><Icon name='protect' /> Team 3</span> },
-]
 
+const options = [{
+  value: '0', image: { avatar: true, src: '../assets/images/selected.png'}
+}]
 
-const NavBar = () => (
-  //TODO: Make links active 
-  <Menu borderless>
-    <Menu.Item position='right'>
-      <Input className='icon' icon='search' placeholder='Search...' size='huge' />
-    </Menu.Item>
-    <Menu.Item>
-      <Icon className='users' size='big' />
-    </Menu.Item>
-    <Menu.Item>
-      <Image src={require('../assets/images/selected.png')} alt='Announcements' avatar />
-    </Menu.Item>
-    <Menu.Item>
-    <Dropdown image='../assets/images/selected.png' options={options}/>
-    </Menu.Item>
-  </Menu>
-)
+class NavBar extends React.Component {
+  render() {
+    return (
+      <Menu borderless>
+      <Menu.Item position='right'>
+        <Input className='icon' icon='search' placeholder='Search...' size='huge' />
+      </Menu.Item>
+      <Menu.Item>
+        <Icon className='users' size='big' />
+      </Menu.Item>
+      <Menu.Item>
+        <Image src={require('../assets/images/selected.png')} alt='Announcements' avatar />
+      </Menu.Item>
+      <Menu.Item>
+        <Dropdown
+          options={4}>
+          <Image src={require('../assets/images/teamimg1.png')} options={options}/>
+        </Dropdown>
+      </Menu.Item>
+    </Menu>
+    )
+  }
+};
 
 export default NavBar;
