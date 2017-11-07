2d85d1991474bd1dc09cc8b41303336e2abdfd72
Patrick Seiuli <patrickseiuli@Patricks-MacBook-Air.local>
2017-11-07 16:40:28 -0700

    Login Border added

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Header, Segment, Form, Button } from 'semantic-ui-react';
+import { Header, Segment, Form, Button, Container } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router';
 import { handleLogin } from '../actions/auth';
@@ -22,39 +22,51 @@ class Login extends Component {
   render() {
     const { email, password, redirect } = this.state;
     return (
-      <div style={{width: '20%', float: 'none', margin: '0 auto'}}>
-        <Segment basic>
-          <Header as='h1' textAlign='center'>Login</Header>
-          <Form onSubmit={this.handleSubmit}>
-            <Form.Field>
-              <label htmlFor='email'>Email</label>
-              <input
-                required
-                id='email'
-                value={email}
-                placeholder='Email'
-                onChange={this.handleChange}
+      <Container>
+      <div class="ui stacked secondary  segment">
+        <div class="field">
+          <Segment basic>
+            <Header as='h1' textAlign='center'>Login</Header>
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Field>
+                <label htmlFor='email'>Email</label>
+                <input
+                  required
+                  id='email'
+                  value={email}
+                  placeholder='Email'
+                  onChange={this.handleChange}
+                />
+              </Form.Field>
+              <Form.Field>
+                <label htmlFor='password'>Password</label>
+                <input
+                  required
+                  id='password'
+                  value={password}
+                  placeholder='Password'
+                  type='password'
+                  onChange={this.handleChange}
               />
-            </Form.Field>
-            <Form.Field>
-              <label htmlFor='password'>Password</label>
-              <input
-                required
-                id='password'
-                value={password}
-                placeholder='Password'
-                type='password'
-                onChange={this.handleChange}
-            />
-            </Form.Field>
-            <Segment textAlign='center' basic>
-              <Button primary type='submit'>Submit</Button>
-            </Segment>
-          </Form>
-        </Segment>
-      </div>
-    )     
+              </Form.Field>
+              <Segment textAlign='center' basic>
+                <Button primary type='submit'>Submit</Button>
+              </Segment>
+            </Form>
+          </Segment>
+        </div>
+          </div>
+      </Container>
+      )     
+    }
   }
+
+const styles = {
+  wrapper: { 
+      justifyContent: 'center', 
+      alignItems: 'center',
+      border: 'rounded',
+       }
 }
 
 
