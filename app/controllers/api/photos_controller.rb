2b16b1d02d24f4ef40350d8a33c1dbeb7dbfa91e
Treven Trujillo <treventrujillo@gmail.com>
2017-11-13 15:25:19 -0700

    started photos

---
+++ app/controllers/api/photos_controller.rb
@@ -0,0 +1,8 @@
+class Api::PhotosController < Api::ApplicationController
+
+  def index
+  end
+  
+end
+
+
--- client/src/components/Photos.js
+++ client/src/components/Photos.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
 
 class Photos extends Component {
+  state = { photos: [] }
+  
+  componentDidMount() {
+    // this.getPhotos();
+  }
+
+  // getPhotos = () => {
+  //   axios.get('/api/posts')
+  //     .then(res => {
+  //       const { data } = res;
+  //       const photos = JSON.parse(res.data.res)
+  //       this.setState({ photos: photos.included })
+  //     });
+  // }
+
   render() {
     return (
       <Header as='h1' textAlign='center'>Photos Component</Header>
@@ -9,4 +24,4 @@ class Photos extends Component {
   }
 }
 
-export default Photos;
+export default connect()(Photos);
