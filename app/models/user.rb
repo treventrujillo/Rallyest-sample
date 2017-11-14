abfda577eaa945e7846a8aeb1b68dc035cf0143c
Treven Trujillo <treventrujillo@gmail.com>
2017-11-14 13:03:36 -0700

    fixed auth routing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+  has_secure_password
+
   def authenticate
     session[:access_token] ? true : false
   end
--- client/src/components/Layouts/DashboardLayout.js
+++ client/src/components/Layouts/DashboardLayout.js
@@ -2,30 +2,33 @@
 import React from 'react';
 import NavBar from '../NavBar';
 import LeftMenu from '../LeftMenu';
+import AuthRoute from '../AuthRoute';
 
 const DashboardLayout =({children, ...rest}) => {
   return (
-    <div className="page page-dashboard">
-      <div id="wrap">
-        <div id="left-menu-container">
-          <div id="left-menu-wrap">
-            <div id="overlay">
-              <LeftMenu/>
+    <AuthRoute>
+     <div className="page page-dashboard">
+        <div id="wrap">
+          <div id="left-menu-container">
+            <div id="left-menu-wrap">
+              <div id="overlay">
+                <LeftMenu/>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="test">
-          <div id="hero">
-            <div id="hero-overlay">
-              <NavBar/>
+          <div class="test">
+            <div id="hero">
+              <div id="hero-overlay">
+                <NavBar/>
+              </div>
             </div>
-          </div>
-          <div id="project-one-container">
-            {children}
+            <div id="project-one-container">
+              {children}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </AuthRoute> 
   );
 }
 
