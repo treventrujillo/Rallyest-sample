dd69547ae0babcda42ee20673213821c52102cf5
Erik McComb <mccomb@Eriks-MacBook-Pro.local>
2017-11-27 14:04:48 -0700

    upload styling

--- client/src/components/Files/FileModal.js
+++ client/src/components/Files/FileModal.js
@@ -1,24 +1,55 @@
 import React, { Component } from 'react';
-import { Accordion, Form, Modal, Button, Input } from 'semantic-ui-react';
+import {
+  Header,
+  Segment,
+  Item,
+  Grid,
+  Image,
+  Button,
+  Modal,
+  Loader,
+  Dimmer,
+  Input,
+  Form,
+  Divider,
+  TextArea,
+  Icon
+ } from 'semantic-ui-react';
 import FileUpload from './FileUpload';
 import upload from '../../assets/images/upload.svg';
 
 class FileModal extends Component {
-  
+
   render() {
     return(
-      <Modal 
+      <Modal
         trigger={
-        <div style={{ 
-          width: '100%', 
-          height: '21vh', 
-          borderRadius: '5px', 
-          border: '1px solid #00AADF',
-          backgroundColor: '#F6F6F6',
-          backgroundImage: `url(${upload})`,
-          backgroundRepeat  : 'no-repeat',
-          backgroundPosition: 'center',
-        }}>   
+          <div
+          style={{
+            width: '100%', 
+            height: '21vh', 
+            borderRadius: '5px', 
+            border: '1px solid #00AADF',
+            backgroundColor: '#F6F6F6',
+          }}
+        >
+          <div style={{ display: 'flex', justifyContent: 'center',}}>
+            <div style={{ display: 'flex', flexDirection: 'column', }}>
+
+              <div style={{ padding: '15px 0px 10px 0px', alignSelf: 'center'}}>
+                <Image src={upload}/>
+              </div>
+
+              <div style={{ color: '#8f8f8f', fontSize: '180%', fontWeight: '100', padding: '3px 10px 3px 10px', alignSelf: 'center'}}> 
+                Upload Files
+              </div>
+
+              <div style={{ color: '#8f8f8f', fontSize: '70%', fontWeight: '100', padding: '10px', alignSelf: 'center'}}>
+                Drag and Drop files here to upload
+              </div>
+
+            </div>
+          </div>
         </div>
       }>
         <Modal.Header>Post a File </Modal.Header>
