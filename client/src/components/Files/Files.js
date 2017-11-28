3771a7552ea89b500ccdeaedd2f89616d68f1b04
Treven Trujillo <treventrujillo@gmail.com>
2017-11-27 17:18:36 -0700

    reorganized file imports

--- client/src/components/Files/Files.js
+++ client/src/components/Files/Files.js
@@ -1,30 +1,33 @@
 /*jshint esversion: 6 */
 import React, { Component } from 'react';
+
 import axios from 'axios'
-import { setHeaders } from '../../actions/headers';
 import { Redirect, Route } from 'react-router';
 import { connect } from 'react-redux';
-import {
-   Header,
-   Segment,
-   Item,
-   Grid,
-   Image,
-   Button,
-   Modal,
-   Loader,
-   Dimmer,
-   Input,
-   Form,
-   Divider,
-   TextArea,
-   Icon
-  } from 'semantic-ui-react';
+
 import FileUpload from './FileUpload';
 import FileAccordion from './FileAccordion';
+
+import { setHeaders } from '../../actions/headers';
 import { setFlash } from '../../actions/flash';
-import LabelForm from './LabelForm';
 import upload from '../../assets/images/upload.svg';
+
+import {
+  Header,
+  Segment,
+    Item,
+    Grid,
+    Image,
+    Button,
+    Modal,
+    Loader,
+    Dimmer,
+    Input,
+    Form,
+    Divider,
+    TextArea,
+    Icon
+} from 'semantic-ui-react';
 
 class Files extends Component {
   state = {
@@ -46,51 +49,6 @@ class Files extends Component {
       files: parsed_files.data,
       labels: parsed_labels.data,
     });
-  }
-  
-  
-  // List all files
-
-  listFiles = (files) => {
-    return files.map( file =>
-      <Segment key={file.id}>
-        <div style={{display: 'flex',}}>
-
-          <div style={{ display: 'flex', width: '10%', justifyContent: 'center'}}>
-            <div style={{ alignSelf: 'center', justifyContent: 'center', paddingRight: '10px'}}>
-              <Image src={require('../../assets/images/adobefile.svg')}/>
-            </div>
-          </div>
-
-          <div style={{display: 'flex', flexDirection: 'column', minWidth:'80%'}}>
-
-            <div style={{flex: 1, fontWeight: 'bold', fontSize: '110%'}}>
-              <span>{file.attributes.name}</span>
-            </div>
-          
-            <div style={{fontStyle: 'italic', fontSize: '90%', }}>
-              <span style={{color: '#8f8f8f'}}>{file.attributes.uploadDate}</span>
-            </div>
-          
-            <div>
-              <Button onClick={ () => this.deleteFile(file.id)}>
-                Delete
-              </Button>
-              {this.editModal(file)}
-            </div>
-          
-          </div>
-
-          <div style={{ display: 'flex',  width: '10%', justifyContent: 'flex-end' }}>
-            <div style={{padding: '5px',}}>
-            
-              <Image
-                src={require('../../assets/images/icon-more.svg')}/>
-            </div>
-          </div>
-        </div>
-      </Segment> 
-    );
   }
   
   // Delete file
@@ -218,14 +176,14 @@ class Files extends Component {
                   </div>
 
                 <div>
-                  {this.listFiles(files)}
+                  {listFiles(files, labels)}
                 </div>
               
               </Grid.Column>
               <Grid.Column width={8}>
                 <LabelForm />
         
-                {this.listLabels(labels)}
+                {listLabels(labels)}
               </Grid.Column>
             </Grid>
           </div>
